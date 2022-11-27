97d1b9a93305c6687d3d1b82c83bda1e1a9f79c7
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2022-11-26 22:17:28 -0300

    01
    Comandos boleanos

--- app/src/main/java/com/example/listacompras/Produto.java
+++ app/src/main/java/com/example/listacompras/Produto.java
@@ -1,4 +1,12 @@
 package com.example.listacompras;
 
 public class Produto {
+    private float preco;
+    private String produto;
+    private boolean urgente;
+
+    public Produto (float preco, String produto, boolean urgente) {
+        this
+    }
 }
+
